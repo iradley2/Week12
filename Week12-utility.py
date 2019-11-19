fe50155cc41dc594790830c65efaa71db41d1cec
Ian Radley <iradley@mymail.mines.edu>
2019-11-18 21:02:03 -0700

    adding LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,8 @@
 def PrintOutput(string):
     print("OUTPUT", string)
+
+def LoadFile(string):
+    contents = open(string)
+    contents = contents.read()
+    contents.split("\n")
+    return contents
