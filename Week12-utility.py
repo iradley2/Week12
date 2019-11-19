2d2691ac092a7012cfb689c4d2534306796f7f63
Ian Radley <iradley@mymail.mines.edu>
2019-11-18 21:05:33 -0700

    adding FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,3 +10,7 @@ def LoadFile(string):
 def UpdateString (string1, string2, index):
     new_string = string1[:index] + string2 + string1[index+1:]
     print(new_string)
+
+def FindWordCount(list1, string):
+    count = list1.count(string)
+    return count
