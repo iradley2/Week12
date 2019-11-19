70ef2433ce6377b271becce0e2414bceac3a73cb
Ian Radley <iradley@mymail.mines.edu>
2019-11-18 21:09:23 -0700

    adding NotIn

--- Week12-utility.py
+++ Week12-utility.py
@@ -34,3 +34,10 @@ def Union(list1, list2):
 def Intersection(list1, list2):
     inter = list(set(list1) & set(list2))
     return inter
+
+def NotIn(list1, list2):
+    not_list = []
+    for i in range(len(list1)):
+        if list1[i] not in list2:
+            not_list.append(list1[i])
+    return not_list
