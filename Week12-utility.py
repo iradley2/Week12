f91c2202c41033112c19bbb271c49586573ecfc0
Ian Radley <iradley@mymail.mines.edu>
2019-11-18 21:03:16 -0700

    adding UpdateString

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,7 @@ def LoadFile(string):
     contents = contents.read()
     contents.split("\n")
     return contents
+
+def UpdateString (string1, string2, index):
+    new_string = string1[:index] + string2 + string1[index+1:]
+    print(new_string)
