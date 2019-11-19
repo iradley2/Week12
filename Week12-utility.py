605c64f5de092cd6c119323af8422a401f13e8dd
Ian Radley <iradley@mymail.mines.edu>
2019-11-18 21:06:24 -0700

    adding ScoreFinder

--- Week12-utility.py
+++ Week12-utility.py
@@ -14,3 +14,15 @@ def UpdateString (string1, string2, index):
 def FindWordCount(list1, string):
     count = list1.count(string)
     return count
+
+def ScoreFinder(list1, list2, string):
+    lower_string = []
+    for i in range(len(list1)):
+        lower_string.append(list1[i].lower())
+    string = string.lower()
+    for i in range(len(list1)):
+        if (string == lower_string[i]):
+            print("OUTPUT",list1[i], "got a score of", list2[i])
+            break
+    else:
+        print("OUTPUT player not found")
